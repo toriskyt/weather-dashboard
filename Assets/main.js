fbcdf1114ceea11df5ad944e72f1c59286cd4214
Tracy Torisky <mommers1@verizon.net>
2021-10-13 15:18:43 -0400

    add new coding

---
+++ Assets/main.js
@@ -0,0 +1,7 @@
+// 88b8ea86d4fcf64ec5b6edf5bb0f8492  = API key for weather-dashboard
+// var APIKey = 88b8ea86d4fcf64ec5b6edf5bb0f8492;
+// var city;
+
+// 
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={APIKey}
+
